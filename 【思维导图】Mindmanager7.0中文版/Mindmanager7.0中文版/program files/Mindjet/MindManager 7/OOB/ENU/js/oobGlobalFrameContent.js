95911324e443579b092e1f6fb71d1a8b177439b4
JudyLime <56984748+judylime@users.noreply.github.com>
2020-10-30 11:44:59 -0700

    markeing learn

---
+++ 【思维导图】Mindmanager7.0中文版/Mindmanager7.0中文版/program files/Mindjet/MindManager 7/OOB/ENU/js/oobGlobalFrameContent.js
@@ -0,0 +1,8 @@
+page.defaultMeetingName="我的会议";
+page.adminTopicName="管理";
+page.dateTopicName="日期";
+page.defaultLeaderName="我的姓名";
+page.attendeesTopicName="与会者";
+page.leaderTopicName="主持人";
+page.agendaTopicName="议程";
+page.title="交互式的快速启动";
---
+++ 【思维导图】Mindmanager7.0中文版/Mindmanager7.0中文版/program files/Mindjet/MindManager 7/OOB/ENU/js/oobLocalizedContent.js
@@ -0,0 +1,11 @@
+buttonText.next="前进 >>";
+buttonText.back="<< 后退";
+buttonText.yes="是";
+buttonText.no="否";
+buttonText.learningCenter="学习中心";
+buttonText.closeButton="关闭";
+buttonText.restart="重新启动";
+page.meetingName="在这里输入会议名称";
+page.meetingDate="在这里输入今天的日期";
+page.leaderName="在这里输入你的姓名";
+page.attendeeName="在这里输入与会者";
